9ce818be1e96a5082434c6158b45dd3e1522177b
Erik Engi <kireerike@gmail.com>
2017-05-11 15:16:56 +0200

    Basic Material UI example with version 0.18.0 added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,68 @@
-import React from 'react'
+// In this file, we create a React component which incorporates components provided by Material-UI.
+import React, {Component} from 'react'
 
-const App = () => <div>Welcome to Razzle.</div>
+// Needed for onTouchTap (http://stackoverflow.com/a/34015469/988941)
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+const muiTheme = getMuiTheme({ // it's optional to pass an object
+	palette: {
+		accent1Color: deepOrange500
+	}
+})
+
+import RaisedButton from 'material-ui/RaisedButton'
+import Dialog from 'material-ui/Dialog'
+import {deepOrange500} from 'material-ui/styles/colors'
+import FlatButton from 'material-ui/FlatButton'
+
+const styles = {
+	container: {
+		textAlign: 'center'
+		, paddingTop: 200
+	}
+}
+
+class App extends Component {
+	constructor(properties, context) {
+		super(properties, context)
+
+		this.state = {
+			open: false
+		}
+	}
+
+	handleRequestClose = () => {
+		this.setState({
+			open: false
+		})
+	}
+
+	handleTouchTap = () => {
+		this.setState({
+			open: true
+		})
+	}
+
+	render() {
+		const standardActions = (
+			<FlatButton label="Ok" primary={true} onTouchTap={this.handleRequestClose}/>
+		)
+
+		return (
+			<MuiThemeProvider muiTheme={muiTheme}>
+				<div style={styles.container}>
+					<h1>Material-UI</h1>
+					<h2>example project</h2>
+					<RaisedButton label="Super Secret Password" secondary={true} onTouchTap={this.handleTouchTap}/>
+
+					<Dialog open={this.state.open} title="Super Secret Password" actions={standardActions} onRequestClose={this.handleRequestClose}>1-2-3-4-5</Dialog>
+				</div>
+			</MuiThemeProvider>
+		)
+	}
+}
 
 export default App
